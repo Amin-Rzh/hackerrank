f349e7b4813de70ea5f1f55647a89aff3de92f12
Amin Raeiszadeh <amin.raeiszadeh@gmail.com>
2019-11-12 20:24:28 +0100

    create the project

--- src/com/hackerrank/problems/BirthdayCakeCandles.java
+++ src/com/hackerrank/problems/BirthdayCakeCandles.java
@@ -3,6 +3,17 @@ package com.hackerrank.problems;
 public class BirthdayCakeCandles {
 
     public static void main(String[] args) {
-
+        int ar[] = {3,2,1,3};
+        int maxHeight = 0;
+        int num = 0;
+        for(int i : ar){
+            if(maxHeight < i){
+                maxHeight = i;
+                num = 1;
+            }else if(maxHeight == i){
+                num++ ;
+            }
+        }
+        System.out.println(num);
     }
 }
